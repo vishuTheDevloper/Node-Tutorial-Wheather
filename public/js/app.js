4d81fcd6704e93b77daeef56bee17bbd8c3b80f5
john <kapild@solidappmaker.com>
2021-12-30 19:51:13 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit',(e) => {
     message1.textContent = "Loading..."
     message2.textContent = ""
     const location = search.value
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
 response.json().then((data) => {
    if (data.error){
     message1.textContent = data.error
@@ -23,4 +23,4 @@ response.json().then((data) => {
 })
 })
 })
-
+// https://weather-app-nodebyvishal.herokuapp.com/ | https://git.heroku.com/weather-app-nodebyvishal.git
